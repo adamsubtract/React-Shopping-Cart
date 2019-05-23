8b4d69a51fec42243286a25839e3c0e125e9f8e5
Adam <adam@Adams-MacBook-Pro.local>
2019-05-22 19:38:02 -0700

    Updates

---
+++ precache-manifest.cf5d45881363ac6a3630829bdbbeec92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bd9b59c008e37f2817b6f0405b5417",
+    "url": "/React-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "3a66858ffb868883165f",
+    "url": "/React-Shopping-Cart/static/css/main.aea987da.chunk.css"
+  },
+  {
+    "revision": "188e0f353c4b3686a685",
+    "url": "/React-Shopping-Cart/static/js/2.01b9c849.chunk.js"
+  },
+  {
+    "revision": "3a66858ffb868883165f",
+    "url": "/React-Shopping-Cart/static/js/main.fe38beea.chunk.js"
+  },
+  {
+    "revision": "121f73f5c3c8e5b3b49a",
+    "url": "/React-Shopping-Cart/static/js/runtime~main.024ff1e5.js"
+  }
+]);
